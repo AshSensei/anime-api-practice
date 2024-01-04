03c8414dedbb33ea0acc098bfb13b781a77cd980
AshSensei <an9simbol@gmail.com>
2024-01-04 11:22:20 -0800

    worked out how to pull title from xml

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 
-const { parseString } = require('xml2js');
-
-
 
 async function getAnimeTitle(animeId) {
     try{
@@ -18,27 +15,24 @@ async function getAnimeTitle(animeId) {
         //console.log('response', response.text());
         response.text()
         .then(data => {
-            console.log('Response Body:', data);
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, 'text/xml');
+        
+            // Access the title of "Naruto" from the parsed XML
+            const titleElement = xmlDoc.querySelector('title');
+            const title = titleElement ? titleElement.textContent : null;
+        
+            console.log('Title:', title);
         })
         .catch(error => {
             console.error('Error reading response body:', error);
         });
-
-
-        //trying to parse the xml data
-        parseString(xmlData, { explicitArray: false, ignoreAttrs: true }, (err, result) => {
-            if (err) {
-                console.error('Error parsing XML', err);
-            } else {
-                // Process the parsed XML data (result) here
-                console.log('Parsed XML:', result);
-            }
-        });
         
+
+    
     } catch (err){
         console.error ('Error in getAnimeTitle', err);
     }
 }
-getAnimeTitle();
 
 // http://api.anidb.net:9001/httpapi?client={str}&clientver={int}&protover=1
